5d69af5da1087b8e7e0adfe0d566d068a8de35bc
dancantor <61066850+dancantor@users.noreply.github.com>
2022-03-21 13:11:33 +0200

    deleted-useless-stuff

--- Front-End/academic-information-system/src/app/app.module.ts
+++ Front-End/academic-information-system/src/app/app.module.ts
@@ -3,29 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PracticeComponentComponent } from './practice/practice-component/practice-component.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
-import { StudentComponentComponent } from './students/student-component/student-component.component';
-import { HomeComponent } from './practice/home/home.component';
-import { IndexStudentComponent } from './students/index-student/index-student.component';
-import { AddStudentComponent } from './students/add-student/add-student.component';
-import { EditStudentComponent } from './students/edit-student/edit-student.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormStudentComponent } from './students/form-student/form-student.component';
-import { StudentFilterComponent } from './students/student-filter/student-filter.component'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    PracticeComponentComponent,
-    StudentComponentComponent,
-    HomeComponent,
-    IndexStudentComponent,
-    AddStudentComponent,
-    EditStudentComponent,
-    FormStudentComponent,
-    StudentFilterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
