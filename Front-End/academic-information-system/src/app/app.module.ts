c8df68bdaf0d6ec1849b47c25df4001a459399d3
dancantor <61066850+dancantor@users.noreply.github.com>
2022-03-24 10:09:59 +0200

    import-material

--- Front-End/academic-information-system/src/app/app.module.ts
+++ Front-End/academic-information-system/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './Models/material/material.module';
+import { MaterialModule } from './Modules/material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
 import { StudentMainPageComponent } from './Pages/student-main-page/student-main-page.component';
