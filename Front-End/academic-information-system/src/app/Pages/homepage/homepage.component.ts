8dd6cae03349b4250eaa4797e2e542dbd61c5d08
Cira Stefan <paulu654@gmail.com>
2022-03-30 20:29:40 +0300

    1

--- Front-End/academic-information-system/src/app/Pages/homepage/homepage.component.ts
+++ Front-End/academic-information-system/src/app/Pages/homepage/homepage.component.ts
@@ -12,7 +12,6 @@ export class HomepageComponent implements OnInit {
 
   constructor(private loginService: LoginService, private router: Router) {}
   ngOnInit(): void {
-    console.log("morti mati");
   }
 
   student = {
