226d8d4c84817e139aea59b17991f080568be2d2
Cira Stefan <72073099+CiraStefan@users.noreply.github.com>
2022-05-11 20:33:25 +0300

    implemented main page student

---
+++ Front-End/academic-information-system/src/app/pages/viewcurriculum/viewcurriculum.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewcurriculum',
+  templateUrl: './viewcurriculum.component.html',
+  styleUrls: ['./viewcurriculum.component.scss']
+})
+export class ViewcurriculumComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- Front-End/academic-information-system/src/app/shared/components/navbar/navbar.component.ts
+++ Front-End/academic-information-system/src/app/shared/components/navbar/navbar.component.ts
@@ -2,22 +2,31 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { StorageService } from '../../services/storage.service';
 import { ProfileInformation } from '../../../Models/student.model';
+import { HttpRequestsService } from '../../services/http-requests.service';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
-  
-  @Input()
+export class NavbarComponent implements OnInit {
+
   user!: ProfileInformation;
 
-  constructor(private router: Router, private storageService: StorageService){  }
+  constructor(private router: Router, private storageService: StorageService, private http: HttpRequestsService){  }
+  ngOnInit(): void {
+    this.http.getProfileInfoById(this.storageService.getUserId() || '', this.storageService.getUserType())
+      .subscribe(result => this.user = result);
+  }
 
 
   logout(): void{
     this.storageService.deleteUserData();
     this.router.navigate(['/login']);
+  }
+
+  goToProfile(): void{
+    console.log('go to profile called' + this.storageService.getUserType());
+    this.router.navigate([`profile`])
   }
 
 }
