0c7bb78ecc99953a6a6de0af62c2c26d6cc8f155
dancantor <61066850+dancantor@users.noreply.github.com>
2022-05-25 01:51:18 +0300

    see-mandatory-disciplines
    - in the page of setting the optional list, show also the mandatory disciplines

--- Front-End/academic-information-system/src/app/Models/course-dto.ts
+++ Front-End/academic-information-system/src/app/Models/course-dto.ts
@@ -1,6 +1,6 @@
 export class CourseDto {
-    Name: string;
-    ProfessorName: string;
-    NrOfCredits: number;
-    Type: string;
+    name: string;
+    professorName: string;
+    nrOfCredits: number;
+    type: string;
 }
